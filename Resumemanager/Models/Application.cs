42c76833b9efe89f6e19b8f0f7adbaeeae6077b5
Edwin <edwinwmacharia@gmail.com>
2022-03-02 11:32:15 +0300

    Add project files.

---
+++ Resumemanager/Models/Application.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Resumemanager.Models
+{
+    public class Application
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Gender { get; set; }
+    }
+}
---
+++ Resumemanager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Resumemanager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
